abcb0d0036a5f24baeca2615cdae5bd14bfef72f
brianegona <begona4038@conestogac.on.ca>
2024-09-23 12:04:36 -0400

    Added pre-release functionality to read emails and print mode message

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -7,13 +7,22 @@
 struct STUDENT_DATA {
     std::string firstName;
     std::string lastName;
+#ifdef PRE_RELEASE
+    std::string email;
+#endif
 };
 
 int main() {
-    // Create a vector to hold student data
     std::vector<STUDENT_DATA> students;
 
-    // Open the file
+    // Print the mode (standard or pre-release)
+#ifdef PRE_RELEASE
+    std::cout << "Running Pre-Release version" << std::endl;
+#else
+    std::cout << "Running Standard version" << std::endl;
+#endif
+
+    // Open the StudentData.txt file
     std::ifstream inputFile("StudentData.txt");
     if (!inputFile) {
         std::cerr << "Unable to open file StudentData.txt";
@@ -22,28 +31,45 @@ int main() {
 
     std::string line;
     while (std::getline(inputFile, line)) {
-        // Create a string stream to parse the line
         std::istringstream ss(line);
         std::string firstName, lastName;
-
-        // Get the first and last names, separated by a comma
         if (std::getline(ss, firstName, ',') && std::getline(ss, lastName)) {
-            // Create a STUDENT_DATA object and add it to the vector
-            STUDENT_DATA student;
-            student.firstName = firstName;
-            student.lastName = lastName;
+            STUDENT_DATA student = { firstName, lastName };
             students.push_back(student);
         }
     }
-
-    // Close the file
     inputFile.close();
 
-    // Debug mode functionality
+    // Print student info in debug mode
 #ifdef _DEBUG
-    std::cout << "Student Information:\n";
     for (const auto& student : students) {
         std::cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << std::endl;
+    }
+#endif
+
+    // Pre-release functionality to read StudentData_Emails.txt
+#ifdef PRE_RELEASE
+    // Open the StudentData_Emails.txt file
+    std::ifstream emailFile("StudentData_Emails.txt");
+    if (!emailFile) {
+        std::cerr << "Unable to open file StudentData_Emails.txt";
+        return 1; // Return an error code
+    }
+
+    size_t index = 0;
+    while (std::getline(emailFile, line) && index < students.size()) {
+        std::istringstream ss(line);
+        std::string email;
+        if (std::getline(ss, students[index].firstName, ',') && std::getline(ss, students[index].lastName, ',') && std::getline(ss, email)) {
+            students[index].email = email;
+        }
+        index++;
+    }
+    emailFile.close();
+
+    // Print student info with emails
+    for (const auto& student : students) {
+        std::cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << ", Email: " << student.email << std::endl;
     }
 #endif
 
